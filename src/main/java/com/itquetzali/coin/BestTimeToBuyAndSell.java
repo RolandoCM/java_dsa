f20f034d4ac17b899a692bbf23a1c89c52ff8dde
rolando <rolando@MacBook-Pro-de-rolando.local>
2026-09-17 00:28:47 -0600

    maxProfit

---
+++ src/main/java/com/itquetzali/coin/BestTimeToBuyAndSell.java
@@ -0,0 +1,23 @@
+package com.itquetzali.coin;
+
+public class BestTimeToBuyAndSell {
+
+    public static void main (String[] args) {
+        int[] prices = {7, 1, 5, 3, 6, 4};
+        int maxProfit = maxProfit(prices);
+        System.out.println("Maximum profit is: " + maxProfit);
+    }
+    public static int maxProfit(int [] prices) {
+        int minPrice = Integer.MAX_VALUE;
+        int maxProfit = 0;
+        for (int i = 0; i < prices.length; i++) {
+            if ( prices[i] < minPrice) {
+                minPrice = prices[i];
+            } else if (prices[i] - minPrice > maxProfit) {
+                maxProfit = prices[i] - minPrice;
+            }
+
+        }
+        return maxProfit;
+    }
+}
